d646ce2fad2bd5765d9839269bac8df068248825
momo <mmko5110@gmail.com>
2019-12-29 20:01:59 +0900

    FloatingActionButtonをつけた

--- app/src/main/java/com/example/realm_sample_app/MainActivity.kt
+++ app/src/main/java/com/example/realm_sample_app/MainActivity.kt
@@ -2,11 +2,20 @@ package com.example.realm_sample_app
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import com.google.android.material.snackbar.Snackbar
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val fab: View = findViewById(R.id.fab)
+        fab.setOnClickListener { view ->
+            Snackbar.make(view, "Fabを押しました！", Snackbar.LENGTH_LONG)
+                .setAction("Action", null)
+                .show()
+        }
     }
 }
