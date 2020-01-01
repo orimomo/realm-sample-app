e768be483f08e8bf0df69a8a07dc1a8b64f97484
momo <mmko5110@gmail.com>
2020-01-01 22:15:24 +0900

    stetho-realmライブラリを使えるようにした
    https://blog.local-c.com/archives/2496

--- app/src/main/java/com/example/realm_sample_app/Application.kt
+++ app/src/main/java/com/example/realm_sample_app/Application.kt
@@ -1,5 +1,7 @@
 package com.example.realm_sample_app
 
+import com.facebook.stetho.Stetho
+import com.uphyca.stetho_realm.RealmInspectorModulesProvider
 import io.realm.Realm
 import io.realm.RealmConfiguration
 
@@ -11,5 +13,11 @@ class Application : android.app.Application() {
         Realm.init(this)
         val config = RealmConfiguration.Builder().build()
         Realm.setDefaultConfiguration(config)
+
+        Stetho.initialize(
+            Stetho.newInitializerBuilder(this)
+                .enableDumpapp(Stetho.defaultDumperPluginsProvider(this))
+                .enableWebKitInspector(RealmInspectorModulesProvider.builder(this).build())
+                .build())
     }
 }
