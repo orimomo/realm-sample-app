51e8721db5737bbf051bf4317d27aa59d1934f5f
momo <mmko5110@gmail.com>
2020-01-04 20:23:57 +0900

    削除の確認ダイアログを表示させた

--- app/src/main/java/com/example/realm_sample_app/ListItem.kt
+++ app/src/main/java/com/example/realm_sample_app/ListItem.kt
@@ -1,5 +1,6 @@
 package com.example.realm_sample_app
 
+import androidx.appcompat.app.AlertDialog
 import com.example.realm_sample_app.databinding.ItemListBinding
 import com.xwray.groupie.databinding.BindableItem
 
@@ -9,8 +10,14 @@ class ListItem(private val obj: ListObject) : BindableItem<ItemListBinding>() {
     override fun bind(viewBinding: ItemListBinding, position: Int) {
         viewBinding.text = obj.title
 
-        viewBinding.deleteButton.setOnClickListener {
-            // 確認ダイアログを表示
+        viewBinding.deleteButton.setOnClickListener { view ->
+            AlertDialog.Builder(view.context)
+                .setMessage("「${obj.title}」のメモを本当に削除しますか？")
+                .setPositiveButton("削除") { _, _ ->
+                    // 処理
+                }
+                .setNegativeButton("キャンセル", null)
+                .create().show()
         }
     }
 }
